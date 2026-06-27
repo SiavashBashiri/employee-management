22abb35f1c920f5e125f557baae73d88cbaae6c3
s.bashiri <siavash.bashiri88@gmail.com>
2026-06-27 11:32:21 +0400

    chore: generate employees/offices/tags modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmployeesModule } from './modules/employees/employees.module';
+import { TagsModule } from './modules/tags/tags.module';
+import { OfficesModule } from './modules/offices/offices.module';
 
 @Module({
-  imports: [],
+  imports: [EmployeesModule, TagsModule, OfficesModule],
   controllers: [AppController],
   providers: [AppService],
 })

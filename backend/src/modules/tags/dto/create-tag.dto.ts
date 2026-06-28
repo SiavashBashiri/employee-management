bd99c73c5dd862f862318cd4bc3548f92595eaea
s.bashiri <siavash.bashiri88@gmail.com>
2026-06-28 13:09:22 +0400

    feat(tags): adding validations to dtos

--- backend/src/modules/tags/dto/create-tag.dto.ts
+++ backend/src/modules/tags/dto/create-tag.dto.ts
@@ -1,7 +1,13 @@
+import { IsEnum, IsHexColor, IsString } from 'class-validator';
 import { TagType } from '../consts/tag-type.const';
 
 export class CreateTagDto {
+  @IsString()
   label: string;
+
+  @IsHexColor()
   color: string;
+
+  @IsEnum(TagType)
   type: TagType;
 }

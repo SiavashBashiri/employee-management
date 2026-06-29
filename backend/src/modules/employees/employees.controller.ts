dcbd7d78e4ce145b253cfb670b9b96587bb2f029
s.bashiri <siavash.bashiri88@gmail.com>
2026-06-29 18:07:01 +0400

    feat(employees): implement employees controller

--- backend/src/modules/employees/employees.controller.ts
+++ backend/src/modules/employees/employees.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { EmployeesService } from './employees.service';
+import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { OfficeLocation } from './consts/office-location.const';
+import { GetEmployeesQueryDto } from './dto/get-employees.query';
 
 @Controller('employees')
-export class EmployeesController {}
+export class EmployeesController {
+  constructor(private readonly employeesService: EmployeesService) {}
+
+  @Post()
+  create(@Body() dto: CreateEmployeeDto) {
+    return this.employeesService.create(dto);
+  }
+
+  @Get()
+  findAll(@Query() query: GetEmployeesQueryDto) {
+    return this.employeesService.findAll(query);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.employeesService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateEmployeeDto) {
+    return this.employeesService.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.employeesService.remove(id);
+  }
+}

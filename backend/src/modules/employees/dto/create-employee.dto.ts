13d61946b0966f123752a36406b9b987746db325
s.bashiri <siavash.bashiri88@gmail.com>
2026-06-29 12:03:02 +0400

    feat(employees): define create employee dto

---
+++ backend/src/modules/employees/dto/create-employee.dto.ts
@@ -0,0 +1,33 @@
+import {
+  IsString,
+  IsDateString,
+  IsEnum,
+  IsArray,
+  IsNumber,
+} from 'class-validator';
+import { OfficeLocation } from '../consts/office-location.const';
+
+export class CreateEmployeeDto {
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsEnum(OfficeLocation)
+  office: string;
+
+  @IsDateString()
+  dateOfBirth: string;
+
+  @IsString()
+  phoneNumber: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  positiveTagIds: string[];
+
+  @IsArray()
+  @IsNumber({}, { each: true })
+  negativeTagIds: number[];
+}

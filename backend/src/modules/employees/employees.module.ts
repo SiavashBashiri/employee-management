5d2b7b750ec1ccbc053bca106d17d9f546451638
s.bashiri <siavash.bashiri88@gmail.com>
2026-06-29 18:06:31 +0400

    feat(employees): implement employees module configs

--- backend/src/modules/employees/employees.module.ts
+++ backend/src/modules/employees/employees.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { EmployeesController } from './employees.controller';
 import { EmployeesService } from './employees.service';
+import { Employee, EmployeeSchema } from './schemas/employee.schema';
+import { Tag, TagSchema } from '../tags/schemas/tag.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Employee.name,
+        schema: EmployeeSchema,
+      },
+      {
+        name: Tag.name,
+        schema: TagSchema,
+      },
+    ]),
+  ],
   controllers: [EmployeesController],
-  providers: [EmployeesService]
+  providers: [EmployeesService],
+  exports: [EmployeesService],
 })
 export class EmployeesModule {}

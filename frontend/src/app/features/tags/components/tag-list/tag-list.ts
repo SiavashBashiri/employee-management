d8f12bc6e995e8a3a15d49f56492d4da8a2ec5a8
s.bashiri <siavash.bashiri88@gmail.com>
2026-07-01 15:37:52 +0400

    feat(tag): implement tag-list feature

--- frontend/src/app/features/tags/components/tag-list/tag-list.ts
+++ frontend/src/app/features/tags/components/tag-list/tag-list.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TagsService } from '../../services/tags-service';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-tag-list',
-  imports: [],
+  imports: [MatTableModule, MatPaginatorModule, MatProgressSpinnerModule],
   templateUrl: './tag-list.html',
   styleUrl: './tag-list.scss',
 })
-export class TagList {}
+export class TagList {
+  public readonly displayedColumns: string[] = ['label', 'color', 'type'];
+  public readonly tagsService = inject(TagsService);
+}

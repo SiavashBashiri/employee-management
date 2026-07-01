5b22a002eb31ed68b4c41bd0a03fab14c0407208
s.bashiri <siavash.bashiri88@gmail.com>
2026-07-01 14:00:48 +0400

    feat(tags): generate essential components

---
+++ frontend/src/app/features/tags/components/tag-list/tag-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tag-list',
+  imports: [],
+  templateUrl: './tag-list.html',
+  styleUrl: './tag-list.scss',
+})
+export class TagList {}
